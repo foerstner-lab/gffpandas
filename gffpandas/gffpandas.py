7e6c8c82d8ec666fca920bbd6fee848fbf47a2fd
VivianMonzon <vivian.monzon@stud-mail.uni-wuerzburg.de>
2018-04-20 09:53:33 +0200

    get_feature_by_attribute - function changed: dict. as in attr_to_columns and then the rows with a given key and key_value can be selected

--- gffpandas/gffpandas.py
+++ gffpandas/gffpandas.py
@@ -1,3 +1,5 @@
+' This is an overall description for my library. '
+
 import pandas as pd
 import itertools
 from collections import defaultdict
@@ -8,6 +10,7 @@ def read_gff3(input_file):
 
 
 class Gff3DataFrame(object):
+    ''' This is a description '''
 
     def __init__(self, input_gff_file=None, input_df=None, input_header=None):
         if input_gff_file is not None:
@@ -26,6 +29,7 @@ class Gff3DataFrame(object):
         return self._df
 
     def _read_gff_header(self):
+        """ This is a function for creating the header. """
         self._header = ''
         for line in open(self._gff_file):
             if line.startswith('#'):
@@ -35,18 +39,28 @@ class Gff3DataFrame(object):
         return self._header
 
     def write_csv(self, csv_file):
+        """ With this function you can write your pandas dataframe to a csv_file. """
         self._df.to_csv(csv_file, sep=',', index=False,
                         header=["seq_id", "source", "feature", "start",
                                 "end", "score", "strand", "phase",
                                 "attributes"])
 
     def write_tsv(self, tsv_file):
+        """ With this function you can write your pandas dataframe to a tsv_file. """
         self._df.to_csv(tsv_file, sep='\t', index=False,
                         header=["seq_id", "source", "feature", "start",
                                 "end", "score", "strand", "phase",
                                 "attributes"])
 
     def filter_feature_of_type(self, type):
+        """ Filtering the dataframe
+
+        For this function a feature-argument is required, as e.x. 'CDS'.
+        As output an object will be created.
+        By printing 'object._df' a filtered dataframe will be returned
+        containing only the datas for the given feature.
+        By printing 'object._df' the header will be returned.
+        """
         feature_df = self._df
         feature_df = feature_df[feature_df.feature == type]
         return Gff3DataFrame(input_df=feature_df, input_header=self._header)
@@ -62,20 +76,24 @@ class Gff3DataFrame(object):
                               input_header=self._header),
                 filtered_length, self._header]
 
-    def get_feature_by_attribute(self, key_locus_tag):
-        gene_df = self._df
-        gene_df = gene_df[gene_df.feature == 'gene']
-        gene_ID = gene_df.attributes.apply(lambda attribute: attribute.split
-                                           ('ID=')[1].split(';')[0])
-        gene_ID_list = gene_ID.tolist()
-        locus_tag = gene_df.attributes.apply(lambda attribute: attribute.
-                                             split('locus_tag=')[1].
-                                             split(';')[0])
-        locus_tag_list = locus_tag.tolist()
-        dictionary = dict(zip(locus_tag_list, gene_ID_list))
-        gene = dictionary.get(key_locus_tag, 'not available')
-        return Gff3DataFrame(input_df=gene)
-        
+    def get_feature_by_attribute(self, attr_key, attr_value=None):
+        attribute_df = self._df
+        attribute_df['at_dic'] = attribute_df.attributes.apply(
+            lambda attributes: dict([key_value_pair.split('=') for
+                                     key_value_pair in attributes.split(';')]))
+        attribute_df['at_dic_keys'] = attribute_df['at_dic'].apply(
+            lambda at_dic: list(at_dic.keys()))
+        merged_attribute_list = list(itertools.chain.
+                                     from_iterable(attribute_df
+                                                   ['at_dic_keys']))
+        nonredundant_list = sorted(list(set(merged_attribute_list)))
+        for atr in nonredundant_list:
+            attribute_df[atr] = attribute_df['at_dic'].apply(lambda at_dic:
+                                                             at_dic.get(atr))
+        filtered_by_attr_df = attribute_df[(attribute_df[attr_key]
+                                            == attr_value)]
+        return Gff3DataFrame(input_df=filtered_by_attr_df)
+            
     def attributes_to_columns(self):
         attribute_df = self._df
         attribute_df['at_dic'] = attribute_df.attributes.apply(
@@ -92,8 +110,9 @@ class Gff3DataFrame(object):
                                                              at_dic.get(atr))
         return Gff3DataFrame(input_df=attribute_df, input_header=self._header)
 
-    def stats_dic(self) -> dict:
+    def stats_dic(self, seq_id=None) -> dict:
         input_df = self._df
+        input_df = input_df[input_df.seq_id == seq_id]
         df_w_region = input_df[input_df.feature != 'region']
         gene_length = pd.Series(df_w_region.apply(lambda row:
                                                   row.end - row.start, axis=1))
@@ -178,3 +197,7 @@ class Gff3DataFrame(object):
 # test_object = read_gff3('NC_016810B.gff')
 # data_frame_new = test_object._read_gff3_to_df()
 # print(data_frame_new)
+
+# print(__doc__)
+# print(Gff3DataFrame.__doc__)
+# print(Gff3DataFrame._read_gff_header.__doc__)
