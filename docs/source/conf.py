8cedfc926c1432161f99683d905f00e3e1cad055
VivianMonzon <vivian.monzon@stud-mail.uni-wuerzburg.de>
2018-05-17 16:22:58 +0200

    logo added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,6 +95,11 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "logo.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
