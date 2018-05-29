0d0b6dd0b60c6625cc08490ade2a89063db4048f
VivianMonzon <vivian.monzon@stud-mail.uni-wuerzburg.de>
2018-05-29 10:19:32 +0200

    changed conf.py for Read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+import shlex
+import sphinx_rtd_theme
 
 
 # -- General configuration ------------------------------------------------
@@ -87,14 +89,17 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {}
 
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
