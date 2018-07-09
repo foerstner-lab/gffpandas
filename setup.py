9b80807c04c65c595b4ed82b49d92eee4302070e
VivianMonzon <vivian.monzon@stud-mail.uni-wuerzburg.de>
2018-07-09 17:38:18 +0200

    only Python3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='gffpandas',
-    version='0.1.0',
+    version='0.1.1',
     description="GFF annotations in panda data frames",
     long_description=readme + '\n\n' + history,
     author="Vivian A. Monzon, Konrad U. Förstner",
@@ -40,17 +40,11 @@ setup(
     zip_safe=False,
     keywords='gffpandas',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements,
