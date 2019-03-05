d3ce412ae9ddef39f486963e32118f5da56bf665
VivianMonzon <vivian.monzon@stud-mail.uni-wuerzburg.de>
2019-03-05 15:54:58 +0100

    try to make API work by giving the gffpandas path to the conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,8 +21,8 @@ import os
 import sys
 import shlex
 import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../gffpandas'))
 
 # -- General configuration ------------------------------------------------
 
