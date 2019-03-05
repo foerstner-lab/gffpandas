bb7c16a892b43d6244cb8a9cd3500d5d4ae0d721
VivianMonzon <vivian.monzon@stud-mail.uni-wuerzburg.de>
2019-03-05 15:42:45 +0100

    try to fix problem for API by giving both paths in conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,9 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
 import shlex
 import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
