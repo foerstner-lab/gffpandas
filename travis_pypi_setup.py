e15f28569f2d54e29f853cf311a5da5dde6fb7d6
Konrad Förstner <konrad@foerstner.org>
2019-03-10 12:16:40 +0100

    Fix username in travis CI config

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'konrad/gffpandas'
+GITHUB_REPO = 'foerstner-lab/gffpandas'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
