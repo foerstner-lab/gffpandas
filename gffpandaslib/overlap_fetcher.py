042dc71399bf48633c5cf546dbe94f967eac5649
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-11-05 22:29:25 +0100

    refactor overlap fetcher using intervals

--- gffpandaslib/overlap_fetcher.py
+++ gffpandaslib/overlap_fetcher.py
@@ -1,4 +1,7 @@
 import logging as lg
+from itertools import product
+
+import pandas as pd
 
 from gffpandaslib.exporter import Exporter
 from gffpandaslib.gff3 import GFF3
@@ -20,70 +23,68 @@ class OverlapFetcher:
 
     def fetch_overlaps(self, allow_different_strands=False):
         lg.info(" Fetching overlaps")
-        for indx in self.input_gff_a.df.index:
-            seq_id = self.input_gff_a.df.at[indx, "seq_id"]
-            a_start = self.input_gff_a.df.at[indx, "start"]
-            a_end = self.input_gff_a.df.at[indx, "end"]
-            a_strand = self.input_gff_a.df.at[indx, "strand"]
-            # Slice gff B
+        self.input_gff_a.df = self.input_gff_a.df.apply(func=lambda row: self._gen_interval(row), axis=1)
+        self.input_gff_b.df = self.input_gff_b.df.apply(func=lambda row: self._gen_interval(row), axis=1)
+        if allow_different_strands:
+            combinations = product(self.input_gff_a.seq_ids)
+        else:
+            combinations = product(self.input_gff_a.seq_ids, ["+", "-"])
+        for comb in combinations:
             if allow_different_strands:
-                tmp_df = self.input_gff_b.df[(self.input_gff_b.df["seq_id"] == seq_id) &
-                                             ((self.input_gff_b.df["start"].between(a_start, a_end)) |
-                                              (self.input_gff_b.df["end"].between(a_start, a_end)))]
+                gff_a_df = self.input_gff_a.df[(self.input_gff_a.df["seq_id"] == comb[0])]
+                gff_b_df = self.input_gff_b.df[(self.input_gff_b.df["seq_id"] == comb[0])]
             else:
-                tmp_df = self.input_gff_b.df[(self.input_gff_b.df["seq_id"] == seq_id) &
-                                             (self.input_gff_b.df["strand"] == a_strand) &
-                                             ((self.input_gff_b.df["start"].between(a_start, a_end)) |
-                                              (self.input_gff_b.df["end"].between(a_start, a_end)))]
-            tmp_df = tmp_df[tmp_df["type"] != 'region']
-            if not tmp_df.empty:
+                gff_a_df = self.input_gff_a.df[(self.input_gff_a.df["seq_id"] == comb[0]) &
+                                               (self.input_gff_a.df["strand"] == comb[1])]
+                gff_b_df = self.input_gff_b.df[(self.input_gff_b.df["seq_id"] == comb[0]) &
+                                               (self.input_gff_b.df["strand"] == comb[1])]
+            for a_indx in gff_a_df.index:
                 counter = 0
-                count_prefix = ""
-                for tmp_indx in tmp_df.index:
-                    counter += 1
-                    b_start = tmp_df.at[tmp_indx, "start"]
-                    b_end = tmp_df.at[tmp_indx, "end"]
-                    b_strand = tmp_df.at[tmp_indx, "strand"]
-                    overlap_size = max(0, min(a_end, b_end) - max(a_start, b_start))
-                    b_attr = self.parse_attributes(tmp_df.at[tmp_indx, 'attributes'])
-                    if 'name' in b_attr.keys():
-                        overlap_name = b_attr["name"]
-                    elif 'label' in b_attr.keys():
-                        overlap_name = b_attr["label"]
-                    elif 'title' in b_attr.keys():
-                        overlap_name = b_attr["title"]
-                    elif 'id' in b_attr.keys():
-                        overlap_name = b_attr["id"]
-                    else:
-                        overlap_name = tmp_df.at[tmp_indx, 'attributes'].replace(";", "-")
-                    if counter > 1:
-                        count_prefix = f"_{counter}"
-                    else:
-                        count_prefix = ""
-                    if not allow_different_strands:
-                        self.input_gff_a.df.at[indx, "attributes"] += \
-                            f";{self.set_b_prefix}_overlap_start{count_prefix}={b_start}" \
-                            f";{self.set_b_prefix}_overlap_end{count_prefix}={b_end}" \
+                for b_indx in gff_b_df.index:
+                    if gff_a_df.at[a_indx, "interval"].overlaps(gff_b_df.at[b_indx, "interval"]):
+                        counter += 1
+                        row = gff_b_df.loc[b_indx].copy()
+                        b_attr = self.parse_attributes(row['attributes'])
+                        if 'name' in b_attr.keys():
+                            overlap_name = b_attr["name"]
+                        elif 'label' in b_attr.keys():
+                            overlap_name = b_attr["label"]
+                        elif 'title' in b_attr.keys():
+                            overlap_name = b_attr["title"]
+                        elif 'id' in b_attr.keys():
+                            overlap_name = b_attr["id"]
+                        else:
+                            overlap_name = row.at['attributes'].replace(";", "-")
+                        if counter > 1:
+                            count_prefix = f"_{counter}"
+                        else:
+                            count_prefix = ""
+                        overlap_size = len(set(list(range(gff_b_df.at[b_indx, 'start'],
+                                                          gff_b_df.at[b_indx, 'end'] + 1, 1))).intersection(
+                            set(list(range(gff_a_df.at[a_indx, 'start'],
+                                           gff_a_df.at[a_indx, 'end'] + 1, 1)))))
+                        self.input_gff_a.df.at[a_indx, "attributes"] += \
+                            f";{self.set_b_prefix}_overlap_start{count_prefix}={gff_b_df.at[b_indx, 'start']}" \
+                            f";{self.set_b_prefix}_overlap_end{count_prefix}={gff_b_df.at[b_indx, 'end']}" \
                             f";{self.set_b_prefix}_overlap_size{count_prefix}={overlap_size}nt" \
                             f";{self.set_b_prefix}_comment{count_prefix}={overlap_name}"
-                        continue
-                    if a_strand == b_strand:
-                        overlap_strand = "sense"
-                    else:
-                        overlap_strand = "antisense"
-                    self.input_gff_a.df.at[indx, "attributes"] += \
-                        f";{self.set_b_prefix}_overlapped_start{count_prefix}={b_start}" \
-                        f";{self.set_b_prefix}_overlapped_end{count_prefix}={b_end}" \
-                        f";{self.set_b_prefix}_overlap_size{count_prefix}={overlap_size}nt" \
-                        f";{self.set_b_prefix}_overlap_strand{count_prefix}={overlap_strand}" \
-                        f";{self.set_b_prefix}_comment{count_prefix}={overlap_name}"
-                counter = 0
-                count_prefix = ""
+                        if allow_different_strands:
+                            if gff_a_df.at[a_indx, 'strand'] == gff_b_df.at[b_indx, 'strand']:
+                                overlap_strand = "sense"
+                            else:
+                                overlap_strand = "antisense"
+                            f";{self.set_b_prefix}_overlap_strand{count_prefix}={overlap_strand}"
+        self.input_gff_a.df.drop("interval", inplace=True, axis=1)
         if self.output_file is not None:
             self._export()
 
     def _export(self):
         Exporter(self.input_gff_a).export_to_gff(self.output_file)
+
+    @staticmethod
+    def _gen_interval(row):
+        row["interval"] = pd.Interval(left=row["start"], right=row["end"])
+        return row
 
     @staticmethod
     def parse_attributes(attr_str):
