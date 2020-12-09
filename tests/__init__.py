78349d27ee62437297f95eeffed2f6e2a69c71fd
Joel Berendzen <joel@generisbio.com>
2020-12-08 17:06:14 -0700

    travis, testing in tmp, human GFF

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
-
 """Unit test package for gffpandas."""
+
+# standard library imports
+import functools
+
+
+def print_docstring():
+    """Decorator to print a docstring."""
+
+    def decorator(func):
+        """Define decorator"""
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            """Print docstring and call function"""
+            print(func.__doc__)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
